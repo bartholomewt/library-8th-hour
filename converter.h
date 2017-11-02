54087bf0d4509f4e95eb2db8d2a02a278771752c
ChloeMcQuin <31670216+ChloeMcQuin@users.noreply.github.com>
2017-11-02 15:05:48 -0500

    Update converter.h

--- converter.h
+++ converter.h
@@ -6,3 +6,12 @@ float cm_to_in(void)
     printf("\nThe number of inches is %.2f\n", in);
     return in;
 }
+
+float in_to_km(void)
+{
+    //Chloe McQuin
+    float in = get_float("Number of inches: ");
+    float km = in * .0000254;
+    printf("\nThe number of kilometers is %.2f\n", km);
+    return km;
+}
