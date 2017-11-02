dcc1c92a1da5b54de58a79624d64608e69eb8796
sergioH10 <31670299+sergioH10@users.noreply.github.com>
2017-11-02 15:04:26 -0500

    Update converter.h

--- converter.h
+++ converter.h
@@ -6,3 +6,13 @@ float cm_to_in(void)
     printf("\nThe number of inches is %.2f\n", in);
     return in;
 }
+float cm_to_feet(void)
+{
+    // Sergio Hinojosa
+    float cm = get_float("Number of Centimeters: ");
+    float feet = cm*0.0328084;
+    printf("\nThe number of feet is %.2f\n", cm);
+    return feet;
+
+
+}
