3d5c7719c4fe90b9c0206dca849851a35e8875df
DacodaB <31670200+DacodaB@users.noreply.github.com>
2017-11-02 15:04:17 -0500

    Update converter.h

--- converter.h
+++ converter.h
@@ -6,3 +6,12 @@ float cm_to_in(void)
     printf("\nThe number of inches is %.2f\n", in);
     return in;
 }
+
+float feet_to_meters(void)
+{
+    //Dacoda Banes
+    float ft = get_float("Number of feet:");
+    float m = ft / 3.28084;
+    printf("\nThe number of meters is %.2f\n", m);
+    return m;
+}
