65343c5aaa2e03c505f6dc683c033d24e03b7353
TylerPamperin <31670230+TylerPamperin@users.noreply.github.com>
2017-11-02 15:04:17 -0500

    Update converter.h

--- converter.h
+++ converter.h
@@ -6,3 +6,12 @@ float cm_to_in(void)
     printf("\nThe number of inches is %.2f\n", in);
     return in;
 }
+
+float in_to_cm (void)
+{
+    //TYLER PAMPERIN
+    float in = get_float("Number of Inches: ");
+    float cm = in * 2.54;
+    printf("\nThe number of centimeters is %.2f\n", cm);
+    return cm;
+}
